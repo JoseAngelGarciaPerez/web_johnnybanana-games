35905c6560668a541d9a86a5d5d85d6c87db0cac
José Ángel García Pérez <73956458+JoseAngelGarciaPerez@users.noreply.github.com>
2025-10-23 18:27:15 +0200

    Componente About añadido
    -Añadido componente About para la página About
    -Header para móviles configurado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Projects } from './projects/projects';
+import { About } from './about/about';
 
 export const routes: Routes = [
     {
         path:'',
-        title: 'App Home Page',
+        title: 'JohnnyBanana Games',
         component: Home,
     },
     
     {
         path:'projects',
-        title: 'Projects',
+        title: 'JohnnyBanana Games - Projects',
         component: Projects,
+    },
+
+    {
+        path: 'about',
+        title: 'JohnnyBanana Games - About',
+        component: About,
     }
 ];
